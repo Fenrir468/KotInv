3509d12eb39abd8d1bb92c1642c47327facd2444
zaegun <gdbarker@gmail.com>
2022-02-02 19:24:03 -0800

    # Fixed Warnings and Some Padding

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,7 +16,7 @@ import java.util.*
 
 
 // CONSTANTS
-const val FORMAT_PADDING = "\n\n"
+const val FORMAT_PADDING = "\n"
 const val NAME_PADDING = 20
 const val ITEM_NAME_PROMPT = "Item Name: "
 const val ITEM_AMOUNT_PROMPT = "Item Amount: "
@@ -42,7 +42,7 @@ class Item(name : String, initAmount : Int){
     fun changeAmount(amount : Int){
         // Method that will change the item amount.
         // This is done through addition, so a negative number is needed to decrement
-        var newAmount = itemAmount + amount
+        val newAmount = itemAmount + amount
         // Sets the new value to the object
         itemAmount = newAmount
     }
@@ -70,7 +70,6 @@ fun showDict() {
     for (item in currentItems) {
         item.getAttr()
     }
-    print(FORMAT_PADDING)
 }
 
 fun addItem() {
@@ -79,23 +78,26 @@ fun addItem() {
 
     // While loop keeps prompting the user until they are done
     while (contAdd) {
+        // Format the text spacing
+        print(FORMAT_PADDING)
+
         // Prompt the user for the item name
         println("What item are you adding to the inventory?")
         print(ITEM_NAME_PROMPT)
-        var itemName = readLine()!!.toString()
+        val itemName = readLine()!!.toString()
 
         // Prompt the user for how many of the item they want to declare
         println("How many of this item do you have?")
         print(ITEM_AMOUNT_PROMPT)
-        var itemAmount = readLine()!!.toInt()
+        val itemAmount = readLine()!!.toInt()
 
         // Create the Item object and add it to the list
-        var newItem = Item(itemName, itemAmount)
+        val newItem = Item(itemName, itemAmount)
         currentItems.add(newItem)
 
         // Ask the user if they want to add more items
         print("Add another Item (Y/N)? ")
-        var selection = readLine()!!.toString()
+        val selection = readLine()!!.toString()
         if (selection.lowercase() == "n") {
             contAdd = false
         }
@@ -105,23 +107,31 @@ fun addItem() {
 
 fun editItem() {
     // This function will prompt the user and allow them to change the item amount
+    // Padding
+    println(FORMAT_PADDING)
+
     // There is a warning that if the item is at 0 or less it will be removed
     println("Note: An item with 0 or less in inventory will automatically be removed from the list")
     println("Which item do you want to modify?")
     print(ITEM_NAME_PROMPT)
-    var findItem = readLine()!!.toString()
+    val findItem = readLine()!!.toString()
 
     // Find the Item
     for (item in currentItems) {
         if (item.itemName == findItem) {
+            // Display Padding
+            print(FORMAT_PADDING)
+
             // Display how many of the item is currently in inventory
-            print("Current Amount in Inventory: ")
+            print("CURRENT AMOUNT: ")
             print(item.itemAmount)
+
             // Prompt the user as to how much they want to increase or decrease the amount
-            println("How much do you want to change the amount by?")
+            println("\nHow much do you want to change the amount by?")
             println("Use positive numbers to increase and negative numbers to decrease")
             print("Amount to Change: ")
-            var changeAmount = readLine()!!.toInt()
+            val changeAmount = readLine()!!.toInt()
+
             // Apply the amount
             item.changeAmount(changeAmount)
             // Do a check to see if the item needs to be removed
@@ -135,13 +145,13 @@ fun main() {
     // While loop controls when the program ends
     while(activeProgram) {
         // Start with the menu and prompt the user for their selection
+        print(FORMAT_PADDING)
         println("Welcome. This is an item list program. Please select one of the following:")
         println("1) View List  2) Add Item  3) Edit Item  4) Help")
         print("Your Selection (x to Exit): ")
-        var selection = readLine()!!.lowercase()
 
         // Based on their selection, perform the following functions
-        when (selection) {
+        when (readLine()!!.lowercase()) {
             "1" -> showDict()     // Displays the current list if it exists
             "2" -> addItem()      // Allows the user to add Items
             "3" -> editItem()     // Allows the user to edit Items
