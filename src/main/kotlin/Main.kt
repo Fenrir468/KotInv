13719359bafe8b15d0509fc8e5d2374a878cbb22
zaegun <gdbarker@gmail.com>
2022-02-02 19:40:43 -0800

    # Bug Fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,26 @@
 import java.util.*
-
-// This is a simply terminal inventory system
-// Load data from JSON and create a list of each item using Item Class
+// This is a simply terminal inventory system built in Kotlin.
+// Loads data from JSON and create a list of each item using Item Class.
 // Main menu has three options:
-//   1) View list, 2) Add Item, 3) Edit Item, 4) Help, 5) Exit
-// Option 1 will display a table of current items in the list
-// Option 2 will prompt for a name and amount
-//   Using the name and amount, use the Class to create an item object
-// Option 3 will prompt for the item name
-//   It will then ask the user how much they want to adjust the item's amount
-//   Positive numbers will increase, and negative numbers will decrease
-//   If the amount is 0 or less, the item is removed
-// Option 4 will display a help prompt
-// Option 'x' Exits the program
+//   1) View list, 2) Add Item, 3) Edit Item, 4) Help, x) Exit
+// Option 1 will display a table of current items in the list.
+// Option 2 will prompt for a name and amount.
+//   Using the name and amount, use the Class to create an item object.
+// Option 3 will prompt for the item name.
+//   It will then ask the user how much they want to adjust the item's amount.
+//   Positive numbers will increase, and negative numbers will decrease.
+//   If the amount is 0 or less, the item is removed.
+// Option 4 will display a help prompt.
+// Option 'x' Exits the program.
 
 
 // CONSTANTS
-const val FORMAT_PADDING = "\n"
-const val NAME_PADDING = 20
-const val ITEM_NAME_PROMPT = "Item Name: "
-const val ITEM_AMOUNT_PROMPT = "Item Amount: "
+const val FORMAT_PADDING = "\n"             // Prints a blank line
+const val NAME_PADDING = 20                 // For formatting the table cell length
 
 // VARIABLES
-var activeProgram = true
-var currentItems = mutableListOf<Item>()
+var activeProgram = true                    // For determining when the user wants to quit the program
+var currentItems = mutableListOf<Item>()    // Holds the list of Items
 
 // CLASSES
 class Item(name : String, initAmount : Int){
@@ -43,8 +40,12 @@ class Item(name : String, initAmount : Int){
         // Method that will change the item amount.
         // This is done through addition, so a negative number is needed to decrement
         val newAmount = itemAmount + amount
+
         // Sets the new value to the object
         itemAmount = newAmount
+
+        // Print the new amount
+        println("Amount for $itemName changed to $itemAmount")
     }
 
     fun getAttr() {
@@ -83,12 +84,12 @@ fun addItem() {
 
         // Prompt the user for the item name
         println("What item are you adding to the inventory?")
-        print(ITEM_NAME_PROMPT)
+        print("Item Name: ")
         val itemName = readLine()!!.toString()
 
         // Prompt the user for how many of the item they want to declare
         println("How many of this item do you have?")
-        print(ITEM_AMOUNT_PROMPT)
+        print("Item Amount: ")
         val itemAmount = readLine()!!.toInt()
 
         // Create the Item object and add it to the list
@@ -111,9 +112,8 @@ fun editItem() {
     println(FORMAT_PADDING)
 
     // There is a warning that if the item is at 0 or less it will be removed
-    println("Note: An item with 0 or less in inventory will automatically be removed from the list")
     println("Which item do you want to modify?")
-    print(ITEM_NAME_PROMPT)
+    print("Item Name: ")
     val findItem = readLine()!!.toString()
 
     // Find the Item
@@ -127,17 +127,15 @@ fun editItem() {
             print(item.itemAmount)
 
             // Prompt the user as to how much they want to increase or decrease the amount
-            println("\nHow much do you want to change the amount by?")
-            println("Use positive numbers to increase and negative numbers to decrease")
+            println("\nHow much do you want to change the amount by? Use negative numbers to decrease.")
             print("Amount to Change: ")
             val changeAmount = readLine()!!.toInt()
 
             // Apply the amount
             item.changeAmount(changeAmount)
-            // Do a check to see if the item needs to be removed
+
         }
     }
-    // Display the current amount
 }
 
 fun main() {
@@ -152,16 +150,17 @@ fun main() {
 
         // Based on their selection, perform the following functions
         when (readLine()!!.lowercase()) {
-            "1" -> showDict()     // Displays the current list if it exists
-            "2" -> addItem()      // Allows the user to add Items
-            "3" -> editItem()     // Allows the user to edit Items
-            "4" -> println("4")   // Displays a help text
-            "x" -> activeProgram = false  // Exits the program
+            "1" -> showDict()               // Displays the current list if it exists
+            "2" -> addItem()                // Allows the user to add Items
+            "3" -> editItem()               // Allows the user to edit Items
+            "4" -> println("4")             // Displays a help text
+            "x" -> activeProgram = false    // Exits the program
             else -> {
                 // If the Selection doesn't match, this is printed
                 println("Please type a number between 1 and 5")
             }
         }
     }
+    // Before the program quits, it informs the user that the program is closing
     println("Closing Program")
 }
